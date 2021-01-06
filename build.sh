70576f6e20cc689284077822299f3e38c3d8aa83
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-01-06 17:34:33 +0530

    Script: Add Initial Script
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#                   GNU GENERAL PUBLIC LICENSE
+#                       Version 3, 29 June 2007
+#
+# Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# Everyone is permitted to copy and distribute verbatim copies
+# of this license document, but changing it is not allowed.
+#
+# Script by @mrjarvis698
+
+. $(pwd)/export.sh
